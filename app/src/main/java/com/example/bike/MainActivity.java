d1ab5191c92a89625289c56f6aacb059ccd21766
PatrykSamelak <patryksamelak1@gmail.com>
2022-06-07 20:40:52 +0200

    add bar, TODO: position

--- app/src/main/java/com/example/bike/MainActivity.java
+++ app/src/main/java/com/example/bike/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.Toolbar;
 
 
 public class MainActivity extends AppCompatActivity implements BikeListFragment.Listener {
@@ -12,6 +13,8 @@ public class MainActivity extends AppCompatActivity implements BikeListFragment.
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     @Override
